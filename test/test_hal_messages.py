3d199d258db3ebc65aa338422b48f6635daab819
Marco Ghibaudi <marco.ghibaudi@riverlane.com>
2021-06-04 09:38:35 +0100

    Removing test that relies on legacy measurement system

--- test/test_hal_messages.py
+++ test/test_hal_messages.py
@@ -31,16 +31,5 @@ class HALTest(unittest.TestCase):
                                     (opcode.name, [arg1, arg0], [qubit1, qubit0])
                                 )
 
-    @unittest.skip("Temporarly disabling this test.")
-    def test_roundtrip_measurements_4q(self):
-        """Test roundtripping of the command packer/unpackers."""
-        test_list = [((Masks.VALIDS.value) | int("0000000000000110", base=2),
-                      [0, 1, 2, 3])]
-        for bitcode, qubits in test_list:
-            rt_bitcode = measurement_creator(
-                measurement_unpacker(bitcode, qubits), qubits)
-            self.assertEqual(rt_bitcode, bitcode)
-
-
 if __name__ == "__main__":
     unittest.main()
